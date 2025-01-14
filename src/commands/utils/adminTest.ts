98f6902fc9acc986d8ac79be83822eaa6f556d63
The-LukeZ <luke.hent3005@gmail.com>
2025-01-14 19:09:01 +0100

    feat: update admin test command to use crypto for random string generation and bump version to 1.2.2

--- src/commands/utils/adminTest.ts
+++ src/commands/utils/adminTest.ts
@@ -1,11 +1,12 @@
 import { ChatInputCommandInteraction, TextChannel } from "discord.js";
+import { randomBytes } from "crypto";
 
 export default async function (ctx: ChatInputCommandInteraction) {
   const messageId = ctx.options.getString("message-id", false);
   await ctx.deferReply({ flags: "Ephemeral" });
 
   if (!messageId) {
-    await ctx.channel.send(`Test ${Randomstring.generate(10)}`);
+    await ctx.channel.send(`Test ${randomBytes(10).toString("hex")}`);
     await ctx.editReply("Sent");
   } else {
     const channel = (await ctx.guild.channels.fetch(
@@ -13,7 +14,7 @@ export default async function (ctx: ChatInputCommandInteraction) {
     )) as TextChannel; // Just to test if it works
     const message = await channel.messages.fetch(messageId);
     console.log("message.client", message.client);
-    await message.edit(`Test ${Randomstring.generate(10)}`);
+    await message.edit(`Test ${randomBytes(10).toString("hex")}`);
     await ctx.editReply("Edited");
   }
 }
