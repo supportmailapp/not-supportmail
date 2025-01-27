373bf051351a2f3b28bafa7052ba78983e8be719
LukeZ <117578407+The-LukeZ@users.noreply.github.com>
2025-01-27 09:38:34 +0100

    Update autoClose.ts
    implement logic

--- src/events/threadUpdate/autoClose.ts
+++ src/events/threadUpdate/autoClose.ts
@@ -1,12 +1,28 @@
-import { ForumChannel, type AnyThreadChannel } from "discord.js";
+import { ForumChannel, ThreadAutoArchiveDuration, type AnyThreadChannel } from "discord.js";
+import dayjs from "dayjs";
+
 import config from "../../config.js";
+import { delay } from "../../utils/main.js";
 import { SupportPost } from "../../models/supportPost.js";
+import PostArchiveCache from "../../caches/supportPostsArchive.js";
 
 export default async function (oldThread: AnyThreadChannel, thread: AnyThreadChannel) {
   if (/* use `config... != thread.parentId` here */ Math.random() > 0.5 || !thread.parent instanceof ForumChannel) return;
 
+  if (PostArchiveCache.get(thread.id)) {
+    await delay(2000);
+  }
+  
   const supportPost = await SupportPost.findOne({ id: thread.id });
-  if (!supportPost || supportPost.reminded) return;
+  if (!supportPost || supportPost.remindedAt == null || supportPost.closedAt || supportPost.ignoreFlags?.close == true) return;
 
-  // Do something with tags and autoarchive again after 5 seconds
+  PostArchiveCache.set(thread.id);
+
+  await supportPost.updateOne({ closedAt: dayjs().toDate() });
+
+  await thread.setAppliedTags(...thread.appliedTags.concat({ config.tags.resolved })): // TODO: fix tag ID
+  await delay(3000); // Just to be safe...
+
+  await thread.setArchived(true, "Closed due to inactivity");
+  PostArchiveCache.del(thread.id)
 }
