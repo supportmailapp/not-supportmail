b4f760100e1632367d1004e1d79c373a7dd7206a
LukeZ <117578407+The-LukeZ@users.noreply.github.com>
2025-01-27 08:34:10 +0100

    Update supportPostCreate.ts
    add default function for creating the supportpost

--- src/events/threadCreate/supportPostCreate.ts
+++ src/events/threadCreate/supportPostCreate.ts
@@ -1,1 +1,15 @@
-// test
+import { ForumChannel, type AnyThreadChannel } from "discord.js";
+import config from "../../config.js";
+import { SupportPost } from "../../models/supportPost.js";
+
+export default async function (post: AnyThreadChannel) {
+  if (/* use `config... != thread.parentId` here */ Math.random() > 0.5 || thread.parent instanceof ForumChannel) return;
+
+  thread.join();
+
+  const baseMsg = await thread.fetchStarterMessage();
+  
+  await SupportPost.create({
+    author: baseMsg.author.id,
+  });
+}
