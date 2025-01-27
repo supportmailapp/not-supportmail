7e313bd6ba19b4f8265772adfbe26bad9c9ec381
The-LukeZ <luke.hent3005@gmail.com>
2025-01-27 10:37:27 +0100

    feat(adminTest): Implement caching for message handling and remove deprecated autoClose and reminder functionalities

--- src/commands/utils/adminTest.ts
+++ src/commands/utils/adminTest.ts
@@ -1,20 +1,35 @@
-import { ChatInputCommandInteraction, TextChannel } from "discord.js";
+import { ChatInputCommandInteraction, Message } from "discord.js";
 import { randomBytes } from "crypto";
+import NodeCache from "node-cache";
+
+// Stores message objects
+let cache = new NodeCache({
+  stdTTL: 300,
+  arrayValueSize: 1000,
+  errorOnMissing: false,
+});
 
 export default async function (ctx: ChatInputCommandInteraction) {
-  const messageId = ctx.options.getString("message-id", false);
+  // const messageId = ctx.options.getString("message-id", false);
   await ctx.deferReply({ flags: "Ephemeral" });
 
-  if (!messageId) {
-    await ctx.channel.send(`Test ${randomBytes(10).toString("hex")}`);
+  const messageData = cache.get(ctx.channelId) as
+    | { message: Message<true> }
+    | undefined;
+
+  if (!messageData || !messageData?.message) {
+    const _message = await ctx.channel.send(
+      `Test ${randomBytes(10).toString("hex")}`
+    );
+    cache.set(ctx.channelId, { message: _message });
     await ctx.editReply("Sent");
   } else {
-    const channel = (await ctx.guild.channels.fetch(
-      ctx.channelId
-    )) as TextChannel; // Just to test if it works
-    const message = await channel.messages.fetch(messageId);
-    console.log("message.client", message.client);
-    await message.edit(`Test ${randomBytes(10).toString("hex")}`);
-    await ctx.editReply("Edited");
+    console.log("message.client", messageData.message.client);
+    await messageData.message
+      .edit(`Test ${randomBytes(10).toString("hex")}`)
+      .catch(console.error);
+    await ctx.editReply(
+      `Edited; message.client exists? ${!!messageData.message.client}`
+    );
   }
 }
