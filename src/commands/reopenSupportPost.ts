eab6a377c7233fac4ff418a48916b86288124644
The-LukeZ <luke.hent3005@gmail.com>
2025-03-11 21:50:27 +0100

    feat(reopen): Add command to reopen resolved support questions

---
+++ src/commands/reopenSupportPost.ts
@@ -0,0 +1,109 @@
+import dayjs from "dayjs";
+import {
+  ChannelType,
+  ChatInputCommandInteraction,
+  type GuildMemberRoleManager,
+  type PermissionsBitField,
+  SlashCommandBuilder,
+  ThreadAutoArchiveDuration,
+} from "discord.js";
+import { SupportPost } from "../models/supportPost.js";
+import config from "../config.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("reopen")
+    .setContexts(0)
+    .setDescription("Reopen a resolved support question.")
+    .addStringOption((op) =>
+      op
+        .setName("reason")
+        .setDescription(
+          "The reason for reopening the question | Is publicly displayed!"
+        )
+        .setMaxLength(128)
+        .setRequired(false)
+    ),
+
+  async run(ctx: ChatInputCommandInteraction) {
+    if (
+      ctx.channel.isDMBased() ||
+      ctx.channel.type !== ChannelType.PublicThread ||
+      ctx.channel.parentId !== process.env.CHANNEL_SUPPORT_FORUM
+    )
+      return await ctx.reply({
+        content: "This is the wrong channel my friend.",
+        flags: 64,
+      });
+
+    if (!ctx.inCachedGuild()) await ctx.guild.fetch();
+
+    const supportPost = await SupportPost.findOne({
+      postId: ctx.channel.id,
+    });
+
+    if (!supportPost) {
+      return await ctx.reply({
+        content: "This post is not a support question.",
+        flags: 64,
+      });
+    } else if (!supportPost.closedAt) {
+      return await ctx.reply({
+        content: "This post is not resolved.",
+        flags: 64,
+      });
+    }
+
+    const memberPermissions = ctx.member
+      .permissions as Readonly<PermissionsBitField>;
+    const canReopenPostRolewise =
+      (ctx.member.roles as GuildMemberRoleManager).cache.hasAny(
+        process.env.ROLE_THREAD_MANAGER,
+        process.env.ROLE_DEVELOPER
+      ) || ctx.user.id == supportPost.author;
+    const canReopenPostPermissionwise =
+      memberPermissions.has("ManageGuild") ||
+      memberPermissions.has("Administrator");
+
+    if (!canReopenPostRolewise && !canReopenPostPermissionwise) {
+      return await ctx.reply({
+        content: `### :x: You are not authorized.\nIt can only be reopened by the author, a staff member or voluntary helper.`,
+        flags: 64,
+      });
+    }
+
+    const tags = ctx.channel.appliedTags;
+    await ctx.channel.edit({
+      appliedTags: [
+        ...tags.filter((tid) => !Object.values(config.tags).includes(tid)),
+        config.tags.unanswered,
+      ],
+      autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+    });
+
+    await supportPost.updateOne({
+      closedAt: null,
+      remindedAt: null,
+      lastActivity: dayjs().toDate(),
+      $unset: {
+        ignoreFlags: "",
+        flags: "",
+      },
+    });
+
+    await ctx.reply({
+      content: "### ✅ Post has been reopened!",
+      embeds: ctx.options.getString("reason")
+        ? [
+            {
+              author: {
+                name: "Reason",
+              },
+              description: ctx.options.getString("reason"),
+              color: 0x2b2d31,
+            },
+          ]
+        : undefined,
+    });
+  },
+};
