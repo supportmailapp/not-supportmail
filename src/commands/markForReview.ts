f4fe53c8b3a92605f09dac53e0a7e786f1e1fa23
LukeZ <117578407+The-LukeZ@users.noreply.github.com>
2025-03-12 10:02:30 +0100

    Update markForReview.ts

--- src/commands/markForReview.ts
+++ src/commands/markForReview.ts
@@ -15,48 +15,39 @@ export default {
   async run(ctx: ChatInputCommandInteraction) {
     if (
       ctx.channel.isDMBased() ||
-      !ctx.inCachedGuild() ||
-      !(
-        ctx.channel.type === ChannelType.PublicThread ||
-        ctx.channel.type === ChannelType.PrivateThread
-      ) ||
-      ctx.channel.parentId !== process.env.CHANNEL_SUPPORT_FORUM ||
-      ctx.channel.parent?.type !== ChannelType.GuildForum
+      ctx.channel.type !== ChannelType.PublicThread ||
+      ctx.channel.parentId !== process.env.CHANNEL_SUPPORT_FORUM
     )
       return await ctx.reply({
         content: "This is the wrong channel my friend.",
         flags: 64,
       });
 
+    if (!ctx.inCachedGuild()) await ctx.guild.fetch();
+
     const supportPost = await SupportPost.findOne({
       postId: ctx.channel.id,
     });
 
-    const canMark = ctx.member.roles.cache.hasAny(
+    if (!supportPost) {
+      return await ctx.reply({
+        content: "This post is not a support post.",
+        flags: 64,
+      });
+    } else if (!supportPost.closedAt) {
+      return await ctx.reply({
+        content: "This post has not been resolved yet.",
+        flags: 64,
+      });
+    }
+
+    const canMarkRolewise = ctx.member.roles.cache.hasAny(
       process.env.ROLE_DEVELOPER,
       process.env.ROLE_THREAD_MANAGER
     );
+    const canMarkPermissionwise = ctx.member.permissions.has("ManageGuild");
 
-    if (!supportPost) {
-      return await ctx.reply({
-        content: "This post is not a support question.",
-        flags: 64,
-      });
-    }
-
-    if (supportPost.closedAt) {
-      return await ctx.reply({
-        content: "This post has already been resolved.",
-        flags: 64,
-      });
-    }
-
-    if (
-      supportPost.author != ctx.user.id &&
-      !canMark &&
-      !ctx.member.permissions.has("ManageGuild") &&
-      !ctx.member.permissions.has("Administrator")
-    ) {
+    if (!canMark && !canMarkPermissionwise) {
       return await ctx.reply({
         content:
           "### :x: You are not authorized.\nIt can only be resolved by the author, a staff member or voluntary helper.",
