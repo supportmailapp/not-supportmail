86856ecfbf944f8ef8e9c4cfecb8ab0c6c3e0022
LukeZ <117578407+The-LukeZ@users.noreply.github.com>
2025-03-12 08:39:55 +0100

    Update autoPublish.ts
    Add functionality for whitelist/blacklist and make more efficient

--- src/events/messageCreate/autoPublish.ts
+++ src/events/messageCreate/autoPublish.ts
@@ -1,26 +1,54 @@
 import { Message, MessageType } from "discord.js";
 import config from "../../config.js";
 
+type ChannelConfig = {
+  pings?: { id: string; type: 1 | 2 }[];
+  whitelist?: { id: string; type: 1 | 2 }[];
+  blacklist?: { id: string; type: 1 | 2 }[];
+  notes?: string;
+};
+
 export default async function autoPublish(message: Message) {
   if (
-    message.guildId !== "1064594649668395128" ||
+    message.guildId !== process.env.GUILD_ID ||
     message.type != MessageType.Default
   )
     return;
 
-  const validChannel = config.autoPublishChannels.find(
+  const channelConfig = config.autoPublishChannels.find(
     (c) => c.id === message.channelId
   );
 
-  let isValidPing = !!validChannel;
-  if (isValidPing) {
-    if (typeof validChannel.pings != "undefined") {
-      isValidPing = validChannel.pings.some((ping) =>
-          ping.type == 1
-            ? message.mentions.users.has(ping.id))
-            : message.mentions.roles.has(ping.id))
+  if (!channelConfig) return;
+
+  let isBlacklisted: boolean = undefined;
+  if (Array.isArray(channelConfig.blacklist))
+      isBlacklisted = channelConfig.blacklist.some((be) =>
+          be.type == 1
+            ? be.id == message.author.id
+            : message.author.roles.cache.has(be.id)
       );
-    }
+
+  if (isBlacklisted === false) return;
+
+  let isWhitelisted: boolean = undefined;
+  if (Array.isArray(channelConfig.whitelist))
+      isWhitelisted = channelConfig.whitelist.some((be) =>
+          be.type == 1
+            ? be.id == message.author.id
+            : message.author.roles.cache.has(be.id)
+      );
+
+  if (isWhitelisted === false) return;
+
+  let isValidPing = true;
+  if (Array.isArray(validChannel.pings)) {
+    isValidPing = false;
+    isValidPing = validChannel.pings.some((ping) =>
+      ping.type == 1
+        ? message.mentions.users.has(ping.id))
+        : message.mentions.roles.has(ping.id))
+    );
   }
 
   if (validChannel && isValidPing) {
