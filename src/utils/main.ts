368c938bd98720fa6a24e1802bdd9334b9ee72db
LukeZ <117578407+The-LukeZ@users.noreply.github.com>
2025-03-12 10:20:29 +0100

    Update main.ts

--- src/utils/main.ts
+++ src/utils/main.ts
@@ -50,7 +50,7 @@ export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export function canUpdateSupportPost(member: GuildMember, authorId = null) {
+export function canUpdateSupportPost(member: GuildMember, authorId: string | null = null) {
   const canRolewise =
     member.roles.cache.hasAny(
       process.env.ROLE_THREAD_MANAGER,
