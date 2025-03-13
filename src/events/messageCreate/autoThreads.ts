61b03075acdf027bdfd14cfe93fa7bdb34a0acf4
The-LukeZ <luke.hent3005@gmail.com>
2025-03-13 15:35:59 +0100

    feat(autoThreads): enhance time handling with AbstractAPI and UTC offset support

--- src/events/messageCreate/autoThreads.ts
+++ src/events/messageCreate/autoThreads.ts
@@ -1,29 +1,29 @@
 import { Message } from "discord.js";
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
 import config from "../../config.js";
 import { checkUserAccess } from "../../utils/main.js";
 
-type ThreadConfig = {
-  schema: string;
-  blacklist?: string[];
-  whitelist?: string[];
-  notes?: string;
-};
+const UTCOffeset = process.env.UTC_OFFSET || null;
 
 export default async function autoThreads(message: Message) {
-  if (message.guildId !== process.env.GUILD_ID || message.author.bot) return;
+  if (
+    message.channel.isDMBased() ||
+    message.channel.isThread() ||
+    message.channel.isVoiceBased() ||
+    message.guildId !== process.env.GUILD_ID ||
+    message.author.bot
+  ) {
+    return;
+  }
 
-  const threadConfig = (config.autoThreadedChannels[message.channelId] ||
-    null) as ThreadConfig | null;
+  const threadConfig = config.autoThreadedChannels[message.channelId] || null;
 
   if (!threadConfig) return;
 
   if (
     !checkUserAccess(
       message.author.id,
-      message.member.roles.cache.map((r) => r.id),
+      message.member?.roles.cache.map((r) => r.id) || [],
       threadConfig.blacklist || [],
       threadConfig.whitelist || []
     )
@@ -31,10 +31,36 @@ export default async function autoThreads(message: Message) {
     return;
   }
 
-  // subsitute any {variables} in threadConfig.schema
-  dayjs.extend(utc);
-  dayjs.extend(timezone);
-  const currentTime = dayjs().tz((config.timezone || "UTC") as any);
+  let currentTime = dayjs();
+  if (process.env.ABSTRACT_API_KEY) {
+    const abstractRes = await fetch(
+      "https://timezone.abstractapi.com/v1/current_time/?" +
+        new URLSearchParams({
+          api_key: process.env.ABSTRACT_API_KEY,
+          location: process.env.LOCATION || "Europe/London",
+        }).toString(),
+      {
+        method: "GET",
+      }
+    );
+
+    if (abstractRes.ok) {
+      const data = await abstractRes.json();
+      currentTime = dayjs(data.datetime, "YYYY-MM-DD HH:mm:ss");
+    } else {
+      console.error(
+        "Error fetching time from AbstractAPI:",
+        abstractRes.status,
+        abstractRes.statusText
+      );
+    }
+  } else if (UTCOffeset) {
+    if (/^\+?\d+$/i.test(UTCOffeset)) {
+      currentTime = currentTime.add(parseInt(UTCOffeset), "h");
+    } else {
+      currentTime = currentTime.subtract(Math.abs(parseInt(UTCOffeset)), "h");
+    }
+  }
 
   const allVariables: { [key: string]: string } = {
     username: message.author.username,
