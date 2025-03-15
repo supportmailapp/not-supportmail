2592f0828e75b421d3772cf0fba608c5b5ed4f0a
The-LukeZ <luke.hent3005@gmail.com>
2025-03-15 22:52:32 +0100

    feat(stats): outsource user stats command

---
+++ src/commands/stats.ts
@@ -0,0 +1,135 @@
+import dayjs from "dayjs";
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+import NodeCache from "node-cache";
+import { FeatureRequest } from "../models/featureRequest.js";
+import { SupportPost } from "../models/supportPost.js";
+import { DBUser } from "../models/user.js";
+import { FeatureRequestStatus } from "../utils/enums.js";
+
+const cache = new NodeCache({
+  stdTTL: 600,
+  checkperiod: 120,
+  errorOnMissing: false,
+});
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("stats")
+    .setContexts(0)
+    .setDescription("View user statistics")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("User to check stats for (defaults to yourself)")
+        .setRequired(false)
+    ),
+
+  async run(ctx: ChatInputCommandInteraction) {
+    if (!ctx.inCachedGuild()) return;
+
+    let targetUser = ctx.options.getUser("user") ?? ctx.user;
+
+    const dbUser = await DBUser.findOne({ id: targetUser.id });
+    if (!dbUser) {
+      await ctx.reply({
+        content: `${
+          targetUser.id === ctx.user.id
+            ? "You haven't"
+            : `${targetUser.username} hasn't`
+        } contributed yet!`,
+        flags: 64,
+      });
+      return;
+    }
+
+    const cacheValue = cache.get(`${ctx.user.id}-${targetUser.id}`) as
+      | string
+      | undefined;
+    if (cacheValue) {
+      await ctx.reply({
+        content: `You are doing this too fast. Try again <t:${dayjs(
+          cacheValue
+        ).unix()}:R>.`,
+        flags: 64,
+      });
+      return;
+    }
+
+    const targetMember = await ctx.guild.members.fetch(targetUser.id);
+
+    // Get feature request stats
+    const featureRequests = await FeatureRequest.find({
+      userId: targetUser.id,
+    });
+    const filterFRs = (status: FeatureRequestStatus) => {
+      return featureRequests
+        .filter((fr) => fr.status === status)
+        .length.toString();
+    };
+    const frStats = {
+      all: featureRequests.length.toString(),
+      implemented: filterFRs(FeatureRequestStatus.Implemented),
+      pending: filterFRs(FeatureRequestStatus.Pending),
+      accepted: filterFRs(FeatureRequestStatus.Accepted),
+      denied: filterFRs(FeatureRequestStatus.Denied),
+    };
+
+    // Get support post stats
+    const supportPosts = await SupportPost.countDocuments({
+      author: targetUser.id,
+    });
+    const helpfulCount = await SupportPost.countDocuments({
+      helped: { $in: [targetUser.id] },
+    });
+
+    await ctx.reply({
+      embeds: [
+        new EmbedBuilder({
+          author: { name: "User Statistics" },
+          title: `${targetMember.displayName || targetUser.username}`,
+          thumbnail: {
+            url: targetUser.avatarURL() || targetUser.defaultAvatarURL,
+          },
+          color: 0xff5733,
+          fields: [
+            {
+              name: "__Bugs Reported__",
+              value: `- \`${dbUser.stats.bugsReported}\``,
+              inline: false,
+            },
+            {
+              name: "__Support Posts__",
+              value: `- Created: \`${supportPosts}\`\n- Helped in: \`${helpfulCount}\``,
+              inline: false,
+            },
+            {
+              name: "__Feature Requests__",
+              value: [
+                "- All: `" + frStats.all + "`",
+                "- Implemented: `" + frStats.implemented + "`",
+                "- Pending: `" + frStats.pending + "`",
+                "- Accepted: `" + frStats.accepted + "`",
+                "- Denied: `" + frStats.denied + "`",
+              ].join("\n"),
+              inline: false,
+            },
+          ],
+          timestamp: new Date().toISOString(),
+        }),
+      ],
+      flags:
+        ctx.channelId !== process.env.BOT_COMMANDS_CHANNEL ? 64 : undefined,
+    });
+
+    if (ctx.channelId === process.env.BOT_COMMANDS_CHANNEL) {
+      cache.set(
+        `${ctx.user.id}-${targetUser.id}`,
+        dayjs().add(10, "minutes").toISOString()
+      );
+    }
+  },
+};
