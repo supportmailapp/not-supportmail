f794a22a4c44b3c2f3df6842027741c9d850d4b9
The-LukeZ <luke.hent3005@gmail.com>
2025-03-15 22:52:15 +0100

    refactor(bugTracker): replace bugHunter command with bugTracker for improved clarity and functionality

---
+++ src/commands/bugTracker.ts
@@ -0,0 +1,75 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { DBUser } from "../models/user.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("bug-tracker")
+    .setContexts(0)
+    .setDescription("Bug hunter commands")
+    .addSubcommand((sub) =>
+      sub
+        .setName("add")
+        .setDescription("Increment bug count for a user")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("The user to increment bug count for")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((sub) =>
+      sub
+        .setName("remove")
+        .setDescription("Decrement bug count for a user")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("The user to decrement bug count for")
+            .setRequired(true)
+        )
+    ),
+
+  async run(ctx: ChatInputCommandInteraction) {
+    if (!ctx.inCachedGuild()) return; // TS bullshit
+    const subcommand = ctx.options.getSubcommand(true);
+    let targetUser = ctx.options.getUser("user", true);
+
+    if (!ctx.member.roles.cache.has(process.env.ROLE_DEVELOPER!)) {
+      await ctx.reply({
+        content: "Nice try! Only developers can award bug-finding glory.",
+        flags: 64,
+      });
+      return;
+    }
+
+    let dbUser = await DBUser.findOne({ id: targetUser.id });
+    if (!dbUser) {
+      dbUser = await DBUser.create({
+        id: targetUser!.id,
+        username: targetUser!.username,
+      });
+    }
+
+    if (subcommand === "add") {
+      await dbUser!.updateOne({
+        $inc: { "stats.bugsReported": 1 },
+      });
+
+      await ctx.reply({
+        content: `Bug count incremented for ${targetUser.username}! Total bugs reported: ${dbUser.stats.bugsReported}`,
+        flags: 64,
+      });
+      return;
+    } else if (subcommand === "remove") {
+      await dbUser!.updateOne({
+        $inc: { "stats.bugsReported": -1 },
+      });
+
+      await ctx.reply({
+        content: `Bug count decremented for ${targetUser.username}! Total bugs reported: ${dbUser.stats.bugsReported}`,
+        flags: 64,
+      });
+      return;
+    }
+  },
+};
