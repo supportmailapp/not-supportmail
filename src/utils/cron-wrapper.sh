a8e74eadfcbb4c43956c31fd9b77da690c753f29
The-LukeZ <luke.hent3005@gmail.com>
2025-03-18 22:45:03 +0100

    fix(cron-wrapper): remove unused cron-wrapper.sh script

--- src/utils/cron-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# cron-wrapper.sh
-echo $PATH
-cd /home/helper/bot
-npm run cron:support-questions
-npm run cron:sync-votes
-# Currently not used:
-# npm run cron:check-subs >> /logs/check-subs.log 2>&1
