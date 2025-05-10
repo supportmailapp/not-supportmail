dbc6dd7e5ea6da1df3d399315695212f0ebdc92f
The-LukeZ <luke.hent3005@gmail.com>
2025-05-11 00:30:46 +0200

    fix(incidents): add status comparison function for incident status validation

--- src/utils/incidents.ts
+++ src/utils/incidents.ts
@@ -1,6 +1,7 @@
 import { HydratedDocument } from "mongoose";
 import * as BetterStack from "./betterstack.js";
 import { IIncident } from "../models/incident.js";
+import { IncidentStatus } from "./enums.js";
 
 const betterstackClient = BetterStack.createBetterStackClient({
   apiKey: process.env.BTSTACK_API_KEY,
@@ -14,4 +15,15 @@ export function incidentURL(incident: HydratedDocument<IIncident>): string {
   } else {
     return `https://status.supportmail.dev/incident/${incident.betterstack.id}`;
   }
+}
+
+export function statusIsEqual(
+  status1: IncidentStatus,
+  status2: BetterStack.ResourceStatus
+) {
+  if (status1 === IncidentStatus.Resolved) {
+    return status2 === "resolved";
+  }
+  if (status1 === IncidentStatus.Maintenance) return status2 === "maintenance";
+  return true;
 }
