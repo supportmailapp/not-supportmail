8b4c11744ec85c588253f7b6d27d4ab37fd71a06
The-LukeZ <luke.hent3005@gmail.com>
2025-06-21 04:42:48 +0200

    fix(config): restructure priority tags for consistency in configuration

--- src/config.ts
+++ src/config.ts
@@ -11,9 +11,11 @@ export default {
     unsolved: process.env.TAG_UNSOLVED,
     solved: process.env.TAG_SOLVED,
     review: process.env.TAG_REVIEW,
-    "priority-P0": process.env.TAG_PRIORITY_P0,
-    "priority-P1": process.env.TAG_PRIORITY_P1,
-    "priority-P2": process.env.TAG_PRIORITY_P2,
+  },
+  priorityTags: {
+    P0: process.env.TAG_PRIORITY_P0,
+    P1: process.env.TAG_PRIORITY_P1,
+    P2: process.env.TAG_PRIORITY_P2,
   },
 } as typeof config & {
   autoThreadedChannels: { [key: string]: ThreadConfig };
@@ -23,9 +25,11 @@ export default {
     unsolved: string;
     solved: string;
     review: string;
-    "priority-P0": string;
-    "priority-P1": string;
-    "priority-P2": string;
+  };
+  priorityTags: {
+    P0: string;
+    P1: string;
+    P2: string;
   };
 };
 
