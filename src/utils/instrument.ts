d3456957883ddc5e8f5750aa3bfda4f419e428a0
The-LukeZ <contact.lukez@proton.me>
2025-12-19 22:06:00 +0100

    fix: adjust Sentry tracing sample rate and enable logs

--- src/utils/instrument.ts
+++ src/utils/instrument.ts
@@ -4,9 +4,7 @@ import * as Sentry from "@sentry/node";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-  _experiments: { enableLogs: true },
+  tracesSampleRate: 0.7,
+  sendDefaultPii: true,
+  enableLogs: true,
 });
